75147441f1360dc803521dc152848b14963ea014
GMConio <poscerbik@gmail.com>
2022-02-23 12:05:07 +0100

    add project

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import pygame
-import time
-import random
+import sys
+
+pygame.init()
+pygame.display.set_caption("Snake")
+screen = pygame.display.set_mode((800, 600))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit(0)
