e7c7fddce70fb7cc7bfbed527fa8a653198c3457
Krzysiek535 <krzysztofkalinski535@gmail.com>
2022-02-23 12:54:19 +0100

    Wyskakiwanie okienka

--- okienko.py
+++ okienko.py
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 import pygame
 import sys
 
+=======
+<<<<<<< HEAD
+
+=======
+import pygame
+import sys
+
+>>>>>>> a6feeb0 (Wyskakiwanie okienka)
 pygame.init()
 
 pygame.display.set_caption("Snake")
@@ -11,3 +20,7 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit(0)
+<<<<<<< HEAD
+=======
+>>>>>>> 0f3a5c3 (Wyskakiwanie okienka)
+>>>>>>> a6feeb0 (Wyskakiwanie okienka)
