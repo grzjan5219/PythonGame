f2f52110e03b490a12ac897ec15ba679acfcc43c
Jarek <jarbil593@gmail.com>
2022-02-24 11:23:17 +0100

    Dodanie przyciska ustawień

--- menu.py
+++ menu.py
@@ -10,9 +10,11 @@ pygame.display.set_caption('Menu ')
 
 start_img = pygame.image.load('img/start.png').convert_alpha()
 exit_img = pygame.image.load('img/exit.png').convert_alpha()
+settings_img = pygame.image.load('img/settings.png').convert_alpha()
 
 start_button = tools.button.Button(100, 200, start_img, 0.8)
 exit_button = tools.button.Button(450, 200, exit_img, 0.8)
+settings_button = tools.button.Button(275, 100, exit_img, 0.8)
 
 screen.fill((200, 200, 250))
 
@@ -23,6 +25,8 @@ while True:
 		print('START')
 	if exit_button.draw(screen):
 		print('EXIT')
+	if settings_button.draw(screen):
+		print('SETTINGS')
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
