89512576dd0ccd72e0b48a731245030a259aa1a7
Krzysiek535 <krzysztofkalinski535@gmail.com>
2022-02-24 13:43:42 +0100

    Poruszanie się węża

--- Snake.py
+++ Snake.py
@@ -14,18 +14,38 @@ game_over = False
 x1 = 400
 y1 = 300
 
+x1_change = 0
+y1_change = 0
+
 clock = pygame.time.Clock()
 
 while not game_over:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       game_over = True
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_LEFT:
+        x1_change = -10
+        y1_change = 0
+      elif event.key == pygame.K_RIGHT:
+        x1_change = 10
+        y1_change = 0
+      elif event.key == pygame.K_UP:
+        x1_change = 0
+        y1_change = -10
+      elif event.key == pygame.K_DOWN:
+        x1_change = 0
+        y1_change = 10
+
+  x1 += x1_change
+  y1 += y1_change
 
   screen.fill(background)
   pygame.draw.rect(screen, snake, [x1, y1, 10, 10])
 
   pygame.display.update()
 
+  clock.tick(30)
 
 pygame.quit()
 quit()
