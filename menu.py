566ea043134613ca41621fee742ea0e0c3b6a0c8
Jarek <jarbil593@gmail.com>
2022-02-25 10:53:01 +0100

    Menu

--- menu.py
+++ menu.py
@@ -5,7 +5,9 @@ from pygame import mixer
 
 def menu():
     pygame.init()
-    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    screen = pygame.display.set_mode((1920, 1080))
+    #, pygame.FULLSCREEN, pygame.RESIZABLE
+    # na razie cofnąłem plik do wersji początkowej, bo z FULLSCREEN nie chciał poprawnie działać
 
     # nazwa okna
     pygame.display.set_caption("Snake")
