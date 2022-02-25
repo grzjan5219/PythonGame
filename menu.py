1a30f5eeea894b67e9c808cfc9ed95242a6a03a4
Kordianzawiszewski <kzawiszewski4@gmail.com>
2022-02-25 11:50:04 +0100

    Menu

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 import pygame
+from Menu import Settings
 from Game.game import Game
 from tools import button
 from pygame import mixer
@@ -25,10 +26,19 @@ def menu():
     start_img = pygame.image.load("img/start.png").convert_alpha()
     settings_img = pygame.image.load("img/settings.png").convert_alpha()
     exit_img = pygame.image.load("img/exit.png").convert_alpha()
+    on_img = pygame.image.load("img/on.png").convert_alpha()
+    off_img = pygame.image.load("img/off.png").convert_alpha()
 
     start_button = button.Button(600, 450, start_img, 0.7)
     settings_button = button.Button(600, 600, settings_img, 0.7)
     exit_button = button.Button(600, 750, exit_img, 0.7)
+    on_button = button.Button(1595, 950, on_img, 0.9)
+    off_button = button.Button(1595, 950, off_img, 0.9)
+
+    # opcja wyciszania dźwięku (nie dokończona)
+    def sound():
+        off_button.draw(screen)
+        mixer.music.set_volume(0)
 
     run = True
     while run:
@@ -43,6 +53,8 @@ def menu():
         if exit_button.draw(screen):
             run = False
             print("exit")
+        if on_button.draw(screen):
+            sound()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
