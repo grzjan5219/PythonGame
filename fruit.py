854bfad150e648544305a8704dfd1b120ed64f50
Jarek <jarbil593@gmail.com>
2022-02-25 11:10:00 +0100

    Menu

--- fruit.py
+++ fruit.py
@@ -1,22 +1,21 @@
 import random
 
 class Food:
-    image = pygame.image.load('img/fruit.png').convert_alpha()
     block_size = None
+    color = (0,255,0)
     x = 0;
     y = 0;
     bounds = None
 
-    def __init__(self, block_size, bounds, image):
+    def __init__(self, block_size, bounds):
         self.block_size = block_size
         self.bounds = bounds
-        self.Food.image = Food.image
 
     def draw(self, game, window):
-        game.draw.rect(window, self.Food.image, (self.x, self.y, self.block_size, self.block_size))
+        game.draw.rect(window, self.color, (self.x, self.y, self.block_size, self.block_size))
 
     def respawn(self):
-        blocks_x = (self.bounds[0])/self.block_size;
-        blocks_y = (self.bounds[1])/self.block_size;
-        self.x = random.randint(0, blocks_x - 1) * self.block_size
-        self.y = random.randint(0, blocks_y - 1) * self.block_size
+        blocks_in_x = (self.bounds[0])/self.block_size;
+        blocks_in_y = (self.bounds[1])/self.block_size;
+        self.x = random.randint(0, blocks_in_x - 1) * self.block_size
+        self.y = random.randint(0, blocks_in_y - 1) * self.block_size
