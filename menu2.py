ca4d3f7da51be9c3e0eb18693beb73d3dfb02e9d
Michał Majchrzak <michalmajchrzak50@gmail.com>
2022-02-25 08:56:57 +0100

    Menu

--- menu2.py
+++ menu2.py
@@ -1,6 +1,6 @@
 import pygame
 from tools import button
-
+from pygame import mixer
 
 def menu():
     pygame.init()
@@ -11,6 +11,12 @@ def menu():
 
     # tło
     tlo_img = pygame.image.load("img/tlo.jpg").convert_alpha()
+
+    #muzyka w tle
+    mixer.music.load("sounds/BG music - menu.mp3")
+    mixer.music.play(-1)
+    mixer.music.set_volume(0.1)
+
 
     # opcje wyboru
     start_img = pygame.image.load("img/start.png").convert_alpha()
