f97db9a0b8f89e914e99801d7ed18ca2ec47b3af
GMConio <poscerbik@gmail.com>
2022-02-28 12:43:48 +0100

    update

--- Game/snake.py
+++ Game/snake.py
@@ -6,13 +6,15 @@ class Snake():
         #print(game.gameBoard.sizeBlock)
         sizeBlock = game.gameBoard.sizeBlock
 
-        self.snake = pygame.Rect(200, 200, sizeBlock-4, sizeBlock-4)
+
         self.length = 1
+        # pozycja w tablicy początku węża
         self.headFieldPos = pygame.math.Vector2(4, (int)(game.gameBoard.height / 2))
+        # pozycja na ekranie
         self.headFieldCord = self.game.gameBoard.boardPos + pygame.math.Vector2(self.headFieldPos.x *sizeBlock, self.headFieldPos.y *sizeBlock)
 
-        print(self.snake.centery)
-
+        self.snake = pygame.Rect(self.headFieldCord.x, self.headFieldCord.y , sizeBlock, sizeBlock)
+        print(self.snake.center)
 
         #print(self.headFieldCord)
         #self.game.gameBoard.boardPos
