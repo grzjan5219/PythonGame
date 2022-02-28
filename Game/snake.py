f1f8cd0c6b897fbb73b806d96bbfddc02d109d8e
Jarek <jarbil593@gmail.com>
2022-02-28 13:05:46 +0100

    snake

--- Game/snake.py
+++ Game/snake.py
@@ -8,7 +8,7 @@ class Snake():
 
         snake_position = [200, 200]
         self.snake_position = snake_position
-        self.snake_body = [[200, 175], [200, 150], [200, 125], [200, 100]]
+        self.snake_body = [[200, 180], [200, 160], [200, 140], [200, 120]]
         self.snake = pygame.Rect(self.snake_position[0], self.snake_position[1], sizeBlock, sizeBlock)
         self.headFieldPos = pygame.math.Vector2(4, (int)(game.gameBoard.height / 2))
         self.headFieldCord = self.game.gameBoard.boardPos + pygame.math.Vector2(self.headFieldPos.x *sizeBlock, self.headFieldPos.y *sizeBlock)
