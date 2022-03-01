19c8cbf7105f54103c2c5f8c9c64ace5e4c4030b
Jarek <jarbil593@gmail.com>
2022-03-01 12:29:26 +0100

    fruit

--- fruit.py
+++ fruit.py
@@ -1,6 +1,5 @@
 import random
 import pygame
-from Game.snake import Snake
 
 class Food():
     def __init__(self, game, bounds = (1920, 1080), block_size = 20):
@@ -15,11 +14,7 @@ class Food():
         self.respawn()
 
     def draw(self):
-        self.snake = Snake(self)
-        if self.snake.snake_position[0] == self.x and self.snake.snake_position[1] == self.y:
-            pygame.draw.rect(self.game.screen, self.color, (self.x, self.y, self.block_size, self.block_size))
-        else:
-            self.respawn()
+        pygame.draw.rect(self.game.screen, self.color, (self.x, self.y, self.block_size, self.block_size))
 
     def respawn(self):
         self.x = random.randint(0, self.blocks_in_x - 20) * self.block_size
