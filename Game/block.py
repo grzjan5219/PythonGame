7a20edc4365f216941515846a4024a417d6099fb
Jarek <jarbil593@gmail.com>
2022-03-08 09:40:06 +0100

    dodanie nowego pliku odpowiedzialnego za pojawianie się przeszkód

---
+++ Game/block.py
@@ -0,0 +1,52 @@
+import pygame
+import random
+from Game.fruitType import FruitType
+from Game.fruit import Fruit
+from pygame import mixer
+
+class Food():
+    def __init__(self, game):
+        self.game = game
+        self.commonFruitImage1 = pygame.transform.scale(pygame.image.load("img/limonka.png"), (self.game.gameBoard.sizeBlock, self.game.gameBoard.sizeBlock))
+        self.commonFruitImage2 = pygame.transform.scale(pygame.image.load("img/cytryna.png"), (self.game.gameBoard.sizeBlock, self.game.gameBoard.sizeBlock))
+        self.fruit_list = [self.commonFruitImage1, self.commonFruitImage2]
+        self.fruits = []
+
+    def spawn(self):
+        for fruit in self.fruits:
+            while True:
+                x = random.randint(0, self.game.gameBoard.width - 1)
+                y = random.randint(0, self.game.gameBoard.height - 1)
+
+                if self.game.gameBoard.fields[x][y].isFree and self.game.gameBoard.fields[x][y].fruitType == FruitType.none:
+                    fruit.changePos(x, y)
+                    self.game.gameBoard.fields[x][y].fruitType = FruitType.common
+                    self.game.gameBoard.fields[x][y].fruit = fruit
+                    break
+
+    def respawn(self, fruit):
+        self.game.gameBoard.fields[fruit.x][fruit.y].fruitType = FruitType.none
+        #self.game.gameBoard.fields[fruit.x][fruit.y].fruit = None
+        while True:
+            x = random.randint(0, self.game.gameBoard.width - 1)
+            y = random.randint(0, self.game.gameBoard.height - 1)
+
+            if self.game.gameBoard.fields[x][y].isFree and self.game.gameBoard.fields[x][y].fruitType == FruitType.none:
+                fruit.changePos(x, y)
+                self.game.gameBoard.fields[x][y].fruitType = fruit.fruitType
+                self.game.gameBoard.fields[x][y].fruit = fruit
+
+                # dźwięk jedzenia
+                eat_sound = mixer.Sound("sounds/Eatting.mp3")
+                eat_sound.set_volume(0.5)
+                eat_sound.play()
+                break
+
+    def add(self, fruitType):
+        self.fruits.append(Fruit(fruitType))
+
+    def draw(self):
+        if self.game.isRun:
+            for fruit in self.fruits:
+                self.game.screen.blit(self.fruit_list[0], self.game.gameBoard.fields[fruit.x][fruit.y].block)
+                #random.choice(self.fruit_list)
