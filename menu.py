65be22821dd1d0d90d73c5b559f0bdb44fabb854
Michał Majchrzak <michalmajchrzak50@gmail.com>
2022-03-16 09:11:22 +0100

    możliwość wyboru między przenikaniem przez ściany a umieraniem przez nią

--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 import pygame
 from Game.game import Game
+from Game.gameMode import GameMode
 from tools import Buttonhover2
 from pygame import mixer
 from tools.pyvidplayer import Video
@@ -24,10 +25,14 @@ class menu():
         self.back_button = Buttonhover2.Button(650, 750, "img/back", 0.8)
         self.on_button = Buttonhover2.Button(1550, 950, "img/on", 1)
         self.off_button = Buttonhover2.Button(1700, 950, "img/off", 1)
+        self.on2_button = Buttonhover2.Button(1150, 540, "img/on", 1)
+        self.off2_button = Buttonhover2.Button(1300, 540, "img/off", 1)
 
         # filmik, który pokazuje się przed samą grą
         self.vid = Video("video/intro.mp4")
         self.vid.set_size((1920, 1080))
+
+        self.gameMode = GameMode.standard
 
         # muzyka w tle
         mixer.music.load("sounds/BG music - menu.mp3")
@@ -56,21 +61,33 @@ class menu():
     def settings(self):
         while True:
             self.screen.blit(self.tlo_settings_img, (0, 0))
-            self.start_button.draw(self.screen)
+            self.dialogue_font = pygame.font.Font('customFont/NeueAachenProBold.TTF', 60)
+            self.dialogue = self.dialogue_font.render("Penetrating through walls: ", 1, (0, 0, 0))
+            self.screen.blit(self.dialogue, (450, 540))
             self.back_button.draw(self.screen)
             self.on_button.draw(self.screen)
             self.off_button.draw(self.screen)
+            self.on2_button.draw(self.screen)
+            self.off2_button.draw(self.screen)
+
             pygame.display.set_caption("Snake - ustawienia")
 
             # Zamiast przycisku start będzie przycisk "Ustawienia wizualne")
-            if self.start_button.tick():
-                self.wizualne()
-                pygame.display.update()
+            if self.on2_button.tick():
+                self.gameMode = GameMode.timeWarp
+                print(self.gameMode)
+                return
+
+            if self.off2_button.tick():
+                self.gameMode = GameMode.standard
+                print(self.gameMode)
+                return
 
             if self.back_button.tick():
+                print("back")
                 return True
 
-            # opcje dźwięku
+                # opcje dźwięku
             if self.on_button.tick():
                 mixer.music.set_volume(0.1)
             if self.off_button.tick():
@@ -109,7 +126,7 @@ class menu():
 
             # start gry
             if self.start_button.tick():
-                game = Game()
+                game = Game(self.gameMode)
                 game.Start()
 
             #  przycisk ustawień w main menu
